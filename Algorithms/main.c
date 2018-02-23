238350096d064ff43d09402c7d913b46c57b10d0
Beelin <linzhipei@xingfeiinc.com>
2018-02-23 16:41:45 +0800

    Initial Commit

---
+++ Algorithms/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Algorithms
+//
+//  Created by Beelin on 2018/2/23.
+//  Copyright © 2018年 Beelin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
